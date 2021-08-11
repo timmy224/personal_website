ead9d561f70fad976d9a74e36d84e449275f4e4e
Timmy <Timmy224@gmail.com>
2021-08-11 13:25:31 -0400

    add doc ready for JS to resolve dropdown toggle not working

--- src/index.js
+++ src/index.js
@@ -4,36 +4,40 @@ import resume from "./assets/TimmyEng_Resume.pdf";
 
 import "./style/main.scss";
 
-$('a[href^="#"]').on("click", function (event) {
-  var target = $(this.getAttribute("href"));
-  if (target.length) {
-    event.preventDefault();
-    $("html, body").stop().animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      500
-    );
+$(document).ready(() => {
+  // scroll
+  $('a[href^="#"]').on("click", function (event) {
+    var target = $(this.getAttribute("href"));
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").stop().animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        500
+      );
+    }
+  });
+  
+  // remove white border for mobile view skills click
+  $(".skills-btn").on("click", (event) => {
+    let el = event.target.querySelector("i");
+    $(el).toggleClass("fa-chevron-up fa-chevron-down");
+    $("[id$=target]").removeClass("skills-border");
+  });
+  
+  // adding resume to html
+  function addResume(pdf) {
+    if (!pdf) return;
+  
+    const resumeButton = document.querySelector(".cta-btn--resume");
+    resumeButton.setAttribute("href", pdf);
   }
+  
+  initSr();
+  initTilt();
+  addResume(resume);
+
 });
 
-// remove white border for mobile view skills click
-$(".skills-btn").on("click", (event) => {
-  let el = event.target.querySelector("i");
-  $(el).toggleClass("fa-chevron-up fa-chevron-down");
-  $("[id$=target]").removeClass("skills-border");
-});
 
-
-
-function addResume(pdf) {
-  if (!pdf) return;
-
-  const resumeButton = document.querySelector(".cta-btn--resume");
-  resumeButton.setAttribute("href", pdf);
-}
-
-initSr();
-initTilt();
-
-addResume(resume);
